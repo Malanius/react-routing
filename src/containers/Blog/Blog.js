fcdcd039545871b46e579d75d580c929c456b3f1
Malanius <malanius@seznam.cz>
2018-09-26 21:51:01 +0200

    Redirect requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch,NavLink } from 'react-router-dom';
+import { Route, Switch, NavLink, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -34,6 +34,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    {/*<Route path="/" component={Posts} />*/}
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         );
