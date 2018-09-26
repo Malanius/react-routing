70c1df46617744147c6f941d89a7b14fc8b9f1ea
Malanius <malanius@seznam.cz>
2018-09-26 18:37:28 +0200

    Initial project

---
+++ src/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE'; //Set auth header for all INSTANCE request types
+
+export default instance;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'; //Set auth header for all request types
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //Edit request config - add headers, etc.
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+}
+);
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    //Edit request config - add headers, etc.
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+}
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
